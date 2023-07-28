9166e903f04fb2ea628452923055d091eb6e0dbe
dayCod <alayarahman07@gmail.com>
2023-07-28 18:21:51 +0700

    Update: Text Comment for BackupRestoreFileAbstract

--- src/Abstract/BackupRestoreFile.php
+++ src/Abstract/BackupRestoreFile.php
@@ -4,5 +4,13 @@ namespace DayCod\ArtisanBackup\Abstract;
 
 abstract class BackupRestoreFile
 {
+    /**
+     * Always returns an object which represents the connection to a MySQL Server
+     * Regardless of it being successful or not.
+     *
+     * @param object|false $databaseConnection = MySQL Server Connection
+     * @param array $tables = Array of tables that you want to backup
+     *
+     */
     abstract public function writeBackupFileFromSQL(object|false $databaseConnection, array $tables) :string;
 }
